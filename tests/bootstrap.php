f55a413f096261ae371c04f8ca8bc2b306de687f
devuri <devuri@users.noreply.github.com>
2025-01-18 16:34:45 -0500

    fix: updates fix the email

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Kora package.
  *
- * (c) Uriel Wilson <uriel@kora.io>
+ * (c) Uriel Wilson <uriel@koraphp.com>
  *
  * The full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
