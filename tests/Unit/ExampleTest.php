a4cba2ee96a2885ed762504311d4393acb029ec1
devuri <devuri@users.noreply.github.com>
2025-01-18 16:51:54 -0500

    chore: update testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,9 +1,23 @@
 <?php
 
+/*
+ * This file is part of the Kora package.
+ *
+ * (c) Uriel Wilson <uriel@koraphp.com>
+ *
+ * The full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Exceptions\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     public function test_example_true_is_true(): void
